b1b9432afed5145ff56a1c5db5989663c5171c2b
Jose Ramírez <82225137+ocxpke@users.noreply.github.com>
2021-04-14 01:48:03 +0200

    Add files via upload

---
+++ ej107.java
@@ -0,0 +1,32 @@
+package trimestre3;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class ej107 {
+
+    public static void main(String[] args) {
+        String frase;
+        Scanner teclado = new Scanner(System.in);
+        Map<String, Integer> texto = new HashMap<>();
+        do {
+            System.out.println("Introduce frases, para salir deja una linea en blanco");
+            System.out.println(">------------------------------------------------------<");
+            frase = teclado.nextLine();
+            String[] partes = frase.split(" ");
+            if (frase.length() != 0) {
+                for (int i = 0; i < partes.length; i++) {
+                    if (texto.containsKey(partes[i])) {
+                        texto.replace(partes[i], texto.get(partes[i]) + 1);
+                    } else {
+                        texto.put(partes[i], 1);
+                    }
+                }
+            }
+        } while (frase.length() != 0);
+        for (String roam : texto.keySet()) {
+            System.out.println("La palabra " + roam + " aparece " + texto.get(roam) + " veces");
+        }
+    }
+}
